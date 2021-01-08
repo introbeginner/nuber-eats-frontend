ebf542ff4e33403f1b72b4786456f46ebed4ef3b
introbeginner <intronovice@gmail.com>
2021-01-08 16:00:09 +0900

    15.9

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery, useReactiveVar } from '@apollo/client';
 import React from 'react';
+import { isLoggedInVar } from './apollo';
 import { LoggedInRouter } from './routers/logged-in-router';
 import { LoggedOutRouter } from './routers/logged-out-router';
 
-const IS_LOGGED_IN = gql`
-  query isLoggedIn {
-    isLoggedIn @clinet
-  }
-`;
-
 function App() {
-  const {data:{isLoggedIn}} = useQuery(IS_LOGGED_IN);
+  // const {data:{isLoggedIn}} = useQuery(IS_LOGGED_IN);
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   
-  return isLoggedIn ? <LoggedInRouter/> : <LoggedOutRouter/>;
+  return isLoggedIn ? <LoggedInRouter/> : <LoggedOutRouter/>
+  
 }
 
 export default App;
